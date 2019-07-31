e10caf949eab63f0ce87083148059426dd7657ef
Ken Newcomb <knewcomb@nd.edu>
2019-07-31 13:26:33 -0400

    Base implementation

--- board.py
+++ board.py
@@ -1,36 +1,79 @@
-class Board:
-
+class Board:    
+    """Represents a Tic-Tac-Toe board."""
+    
     def __init__(self):
-        self.board = [0 for i in range(9)]
+        self.board = [' ' for i in range(9)]
+        self.broken_rules = False
 
+    def is_done(self):
+        return self.broken_rules or self.is_win() or self.board.count('x')+self.board.count('o') == 9
+
+    def valid_moves(self):
+        moves = []
+        for b in range(len(self.board)):
+            if self.board[b] == ' ':
+                moves.append(b)
+        return moves
+            
     def place(self, loc, t):
-        self.board[loc] = t 
+        if self.board[loc] != ' ':
+            self.broken_rules = True
+        else:
+            self.board[loc] = t 
 
-    def is_win(self):
-        for t in ['x', 'o']:
-            # Horizontal wins
-            if self.board[0] == t and self.board[1] == t and self.board[2] == t:
-                return True
-            if self.board[3] == t and self.board[4] == t and self.board[5] == t:
-                return True
-            if self.board[6] == t and self.board[7] == t and self.board[8] == t:
-                return True
-            # Vertical wins
-            if self.board[0] == t and self.board[3] == t and self.board[6] == t:
-                return True
-            if self.board[1] == t and self.board[4] == t and self.board[7] == t:
-                return True
-            if self.board[2] == t and self.board[5] == t and self.board[8] == t:
-                return True
-            # Diagonal wins
-            if self.board[0] == t and self.board[4] == t and self.board[8] == t:
-                return True
-            if self.board[2] == t and self.board[4] == t and self.board[6] == t:
-                return True
+    def reward(self, player):
+        if self.is_win(player):
+            return 1
+        elif self.broken_rules:
+            return -1
+        elif self.is_done():
+            return 0.5
+        else:
+            return 0.1
+
+    def is_win(self, t = ' '):
+        # Horizontal wins
+        if t == ' ':
+            return self.is_win('x') or self.is_win('o')
+        if self.board[0] == t and self.board[1] == t and self.board[2] == t:
+            return True
+        if self.board[3] == t and self.board[4] == t and self.board[5] == t:
+            return True
+        if self.board[6] == t and self.board[7] == t and self.board[8] == t:
+            return True
+
+        # Vertical wins
+        if self.board[0] == t and self.board[3] == t and self.board[6] == t:
+            return True
+        if self.board[1] == t and self.board[4] == t and self.board[7] == t:
+            return True
+        if self.board[2] == t and self.board[5] == t and self.board[8] == t:
+            return True
+
+        # Diagonal wins
+        if self.board[0] == t and self.board[4] == t and self.board[8] == t:
+            return True
+        if self.board[2] == t and self.board[4] == t and self.board[6] == t:
+            return True
+
         return False
 
     def get_board(self):
         return self.board
+    
+    def get_board_vector(self, t):
+        vec = []
+        for b in self.board:
+            # Your spots
+            if b == t:
+                vec.append(1)
+            # Empty spots
+            elif b == ' ':
+                vec.append(0)
+            # Opponent spots
+            else:
+                vec.append(-1)
+        return vec
 
     def print_board(self):
         print("""{0} | {1} | {2}\n{3} | {4} | {5}\n{6} | {7} | {8}""".format(self.board[0], self.board[1], self.board[2], self.board[3],
--- play.py
+++ play.py
@@ -1,14 +1,94 @@
 '''play.py: Play a game of tic-tac-toe against an AI opponent.'''
 import board
-import sklearn as skl
+import numpy as np
+from sklearn.neural_network import MLPRegressor
+import random
+import time
 
-# Initialize ML model.
-x = []
-y = []
+def experience_replay(replay_memory):
+    """Trains model on experiences, e."""
+    """ e = (s, a, r, s') """
 
-# Run n episodes.
-for n in range(0, 10):
-    board = board.Board()
-    while not board.is_win():
-        for player in range(2):
-            model.fit
+    # Shuffle the memory.
+    random.shuffle(replay_memory)
+
+    # Fit the model to replay memory.
+    states  = []
+    targets = []
+    for (state, action, reward, next_state, done) in replay_memory:
+        if not done:
+            target = reward + gamma*np.max(model.predict([next_state]))
+        else:
+            target = reward
+
+        target_f = model.predict([state])
+        target_f[0][action] = target
+        states.append(state)
+        targets.append(target_f[0])
+    model.fit(states, targets)
+    return model
+
+# Suppress scikit's convergence warnings.
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
+
+replay_memory = []
+gamma = 0.99 # reward decay
+epsilon = 0.50 # exploration/exploitation ratio
+episodes = 5000
+
+# Init ML model
+model = MLPRegressor(hidden_layer_sizes=(100), max_iter=1000)
+xstate = np.zeros(9)
+ystate = np.zeros(9)
+model.fit([xstate], [ystate])
+
+# Play n episodes
+for n in range(episodes):
+    print("Episode #{0}".format(n))
+    print("------------")
+    b = board.Board()
+    total_reward = 0
+    while not b.is_done():
+        for player in ['x', 'o']:
+
+            # Limit experience replay memory length
+            #replay_memory = replay_memory[-7500:]
+
+            # Get current game state
+            state = b.get_board_vector(player)
+
+            # Select action with epsilon-greedy strategy
+            if random.uniform(0, 1) < epsilon:
+                # Explore: make random valid move
+                action = random.choice(b.valid_moves())
+            else:
+                # Exploit: make best move.
+                action = np.argmax(model.predict([state]))
+
+            b.place(action, player)
+            reward = b.reward(player)
+            total_reward += reward
+            new_state = b.get_board_vector(player)
+            
+            # Store experience e(s, a, r, s') in replay memory.
+            replay_memory.append((state, action, reward, new_state, b.is_done()))
+            
+            if b.is_done():
+                break 
+
+    # Update model every so often.
+    if not n % 50:
+        model = experience_replay(replay_memory)
+        with open('out', 'a') as f:
+            f.write("{0} {1}\n".format(n/50, total_reward))
+
+    if b.broken_rules:
+        print("Broken")
+    elif b.is_win('x') or b.is_win('o'):
+        print("Win")
+    else:
+        print("Tie")
+    
